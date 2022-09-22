5ab8b8d41103e319ed1e1d469475326d590df3ab
wofeitosa <wofeitosa@yahoo.com.br>
2022-09-22 19:01:24 -0300

    :sparkles:created scr folder with Html, Css, Js.

---
+++ src/phases.js
@@ -0,0 +1,81 @@
+let phases = [
+    {
+        titulo: 'SENADOR',
+        numeros: 3,
+        candidatos: [
+            {
+                numero: 381,
+                nome:'Fulano de Tal',
+                partido:'ABC',
+                fotos:[
+                    {url:'candidato-5.jpeg', legenda: 'Senador'}
+                ]
+
+            },
+            {
+                numero: 772,
+                nome: 'SENADOR',
+                partido: 'DEF',
+                fotos:[
+                    {url:'candidato-6.jpeg', legenda: 'Senador'}
+                ]
+            },
+        ]
+        
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+        {
+            numero: 77,
+            nome:'Ciclano',
+            partido:'ABC',
+            vice: 'Cic',
+            fotos: [
+                {url:'candidato-1.jpeg', legenda: 'Presidente'},
+                {url:'candidato-2.jpeg', legenda: 'Vice-Presidente', small: true}
+                
+            ]
+
+        },
+        {
+            numero: 86,
+            nome: 'Zulano',
+            partido: 'FTE',
+            vice: 'Zul',
+            fotos: [
+                {url:'candidato-3.jpeg', legenda: 'Presidente'},
+                {url:'candidato-4.jpeg', legenda: 'Vice-Presidente', small: true}
+            ]    
+        },
+        
+        ] 
+    },
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+]
